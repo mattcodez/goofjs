16c313c34f8039c5bb89d58dc9832cbb029aa957
Matthew Molnar <matthew.molnar@gmail.com>
2015-05-01 00:10:00 -0400

    Fix syntax issues with words.js Basic layout AST is displaying in output pane

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$('#compute').on('click', function(e){
+  compute(
+    $('#sourceInput').text(),
+    function(newCode){
+      $('#sourceOutput').text(newCode);
+    }
+  ); //TODO: Can we pipe this at some point?
+});
 
+function compute(source, callback){
+  var tolerant = [];
+  callback(
+    JSON.stringify(
+      esprima.parse(source, {tolerant:tolerant}),
+    null, 4)
+  );
+  console.log(tolerant);
+}
