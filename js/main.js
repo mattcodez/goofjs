59ad11b5f2a1d2a8e8a63d5d846a5edeeb5aa596
Matthew Molnar <matthew.molnar@gmail.com>
2015-05-01 00:58:57 -0400

    Panes for debugging/development.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 $('#compute').on('click', function(e){
   compute(
     $('#sourceInput').text(),
-    function(newCode){
-      $('#sourceOutput').text(newCode);
+    function(codes){
+      $('#sourceOutput').text(codes.source);
+      $('#ASTOutput').text(codes.AST);
+      $('#errorOutput').text(codes.errors);
     }
   ); //TODO: Can we pipe this at some point?
+});
+
+var outputPanes = $('#sourceOutput, #ASTOutput, #errorOutput');
+var paneButtons = $('BUTTON#sourcePane, BUTTON#ASTPane, BUTTON#errorPane')
+paneButtons.on('click', function(e){
+  outputPanes.hide();
+  $('#' + $(e.target).attr('data-pane')).show();
 });
 
 function compute(oldSource, callback){
   var tolerant = [];
 
   var AST = esprima.parse(oldSource, {tolerant:tolerant});
-  console.log(tolerant);
-
   AST.body[0].id.name = 'moo';
 
   var newSource = escodegen.generate(AST);
-  callback(newSource);
+  callback({
+    source:newSource,
+    AST:JSON.stringify(AST,null, 4),
+    errors:JSON.stringify(tolerant,null, 4)
+  });
 }
